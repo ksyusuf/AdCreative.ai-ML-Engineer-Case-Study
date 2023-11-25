4dbffeaad902a42106d091998b2592262d506ae5
ksyus-h <ksyusuf85@gmail.com>
2023-11-25 23:48:17 +0300

    sunucu ip ve port

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import base64
 from io import BytesIO
 from flask import Flask, render_template, request
 import main
-from PIL import Image
+import os
+import socket
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -12,6 +13,32 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/')
 def index():
+
+    def get_ip_address():
+        # Socket oluştur
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        try:
+            # Google'ın DNS sunucusuna bağlan
+            s.connect(('8.8.8.8', 80))
+            ip_address = s.getsockname()[0]
+        except socket.error:
+            ip_address = '127.0.0.1'  # Eğer bağlantı kurulamazsa varsayılan IP adresi
+
+        finally:
+            s.close()
+
+        return ip_address
+
+    def get_port():
+        port = int(os.environ.get('PORT', 8000))
+        return port
+
+    ip_address = get_ip_address()
+    port = get_port()
+
+    print(f"Sunucu IP adresi: {ip_address}")
+    print(f"Kullanılan port: {port}")
     return render_template('index.html')
 
 
