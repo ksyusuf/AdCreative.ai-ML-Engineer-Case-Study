fcb214c13f2aa6bc91eec767ab6d7a6714c0c09a
ksyus-h <ksyusuf85@gmail.com>
2023-11-25 23:45:44 +0300

    sunucu ip ve port

--- Uzak_Baglanti.py
+++ Uzak_Baglanti.py
@@ -13,11 +13,34 @@ class UzakBaglanti:
         self.server_url = f"http://{ip_adresi}:{port}/process_image"
         # Uzak bilgisayarın IP adresi ve portunu güncelle
 
-        # image.save(os.path.join("uploads", image.filename))
-        # # kodlarım dosya yolu ile çalıştığı için
-        # # gelen resimleri önce kaydettim sonra yollarını aldım
-        #
-        # self.file_path = os.path.join("uploads", image.filename)
+        import os
+        import socket
+
+        def get_ip_address():
+            # Socket oluştur
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+            try:
+                # Google'ın DNS sunucusuna bağlan
+                s.connect(('8.8.8.8', 80))
+                ip_address = s.getsockname()[0]
+            except socket.error:
+                ip_address = '127.0.0.1'  # Eğer bağlantı kurulamazsa varsayılan IP adresi
+
+            finally:
+                s.close()
+
+            return ip_address
+
+        def get_port():
+            port = int(os.environ.get('PORT', 8000))
+            return port
+
+        ip_address = get_ip_address()
+        port = get_port()
+
+        print(f"Sunucu IP adresi: {ip_address}")
+        print(f"Kullanılan port: {port}")
 
         self.image = image
         self.prompt = prompt
